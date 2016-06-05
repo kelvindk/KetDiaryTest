6b91bd407b4ece411fc1a23db2e0cdfe38127bd0
Li Cheng Yuan <kelvindk@Kelvin-Li.local>
2016-06-06 04:47:25 +0800

    first push

---
+++ MyApplication/app/src/main/java/ubicomp/myapplication/MainActivity.java
@@ -0,0 +1,97 @@
+package ubicomp.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+import ubicomp.myapplication.toolbar.ToolbarMenuItemAdapter;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    public final static int SPINNER_TEST = 0;
+    public final static int SPINNER_RESULT = 1;
+    public final static int SPINNER_EVENT = 2;
+    public final static int SPINNER_RANKING = 3;
+
+    private Menu menu = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Enable toolbar on main activity
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Get the spinner on the toolbar
+        Spinner spinner_toolbar = (Spinner) findViewById(R.id.spinner_toolbar);
+        spinner_toolbar.setOnItemSelectedListener(this);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> spinner_content_adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, R.layout.spinner_layout);
+        spinner_content_adapter.setDropDownViewResource(R.layout.spinner_layout);
+        // Apply the adapter to the spinner
+        spinner_toolbar.setAdapter(spinner_content_adapter);
+        // Disable the App title on toolbar
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // Create a TabLayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.activity_main_tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+
+        return true;
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        switch(pos) {
+            case SPINNER_TEST:
+                Toast.makeText(this, "pos 0", Toast.LENGTH_SHORT).show();
+                // Inflate the menu; this adds items to the action bar if it is present.
+                ToolbarMenuItemAdapter.inflate(this, R.menu.menu_test, this.menu);
+                break;
+            case SPINNER_RESULT:
+                Toast.makeText(this, "pos 1", Toast.LENGTH_SHORT).show();
+                break;
+            case SPINNER_EVENT:
+                Toast.makeText(this, "pos 2", Toast.LENGTH_SHORT).show();
+                ToolbarMenuItemAdapter.inflate(this, R.menu.menu_event, this.menu);
+                break;
+            case SPINNER_RANKING:
+                Toast.makeText(this, "pos 3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
---
+++ MyApplication/app/src/main/java/ubicomp/myapplication/toolbar/ToolbarMenuItemAdapter.java
@@ -0,0 +1,69 @@
+package ubicomp.myapplication.toolbar;
+
+import android.support.v4.view.MenuItemCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import ubicomp.myapplication.MainActivity;
+import ubicomp.myapplication.R;
+
+/**
+ *  ToolbarMenuItemAdapter is used to handle the page switching between test, result, event and
+ *  ranking pages. The click listener of each button on toolbar is also in this class.
+ *
+ * Created by kelvindk on 16/6/6.
+ */
+public class ToolbarMenuItemAdapter {
+
+    public static void inflate(final MainActivity mainActivity, int menuID, Menu menu) {
+
+        if(menu == null)
+            return;
+        menu.clear();
+        switch (menuID) {
+            case R.menu.menu_test:
+
+                mainActivity.getMenuInflater().inflate(R.menu.menu_test, menu);
+
+                MenuItem itemActionSettings = menu.findItem(R.id.action_settings);
+                MenuItemCompat.getActionView(itemActionSettings).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(mainActivity, "action_settings", Toast.LENGTH_SHORT).show();
+                        // Listener of action Settings button
+                    }
+                });
+                break;
+            case R.menu.menu_event:
+                mainActivity.getMenuInflater().inflate(R.menu.menu_event, menu);
+
+                MenuItem actionAddEvent = menu.findItem(R.id.action_add_event);
+                MenuItemCompat.getActionView(actionAddEvent).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(mainActivity, "action_add_event", Toast.LENGTH_SHORT).show();
+                        // Listener of action Add button
+                    }
+                });
+
+                MenuItem actionRemind = menu.findItem(R.id.action_remind);
+                MenuItemCompat.getActionView(actionRemind).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Listener of action Remind button
+                        TextView remindBadgeText = (TextView) mainActivity.findViewById(R.id.menu_remind_badge);
+                        remindBadgeText.setText(""+(Integer.parseInt(remindBadgeText.getText().toString())+1));
+                        Toast.makeText(mainActivity, "action_remind "+remindBadgeText.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                break;
+        }
+
+
+    }
+}
